fe3916925ad50eeb03f96f0da838e965ce86b203
G00407095@atu.ie <G00407095@atu.ie>
2024-10-16 16:27:56 +0100

    forgot to add branch :((( added some extra methods for first hashmap added hash map that takes in string key

--- src/main/java/ie/atu/practice/HashMapDemo.java
+++ src/main/java/ie/atu/practice/HashMapDemo.java
@@ -14,27 +14,29 @@ public class HashMapDemo {
             myMap.put(i, baseValue);
             baseValue += 100;
         }
-
         System.out.println(" print hashmap " + myMap);
-
         System.out.println("enter new number: ");
         int number = sc.nextInt();
         myMap.put(11, number);
-
         System.out.println("size " + myMap.size());
-
+        // shows all keys and values
         System.out.println("entry set " + myMap.entrySet());
-
+        // shows only the keys
         System.out.println("key set " + myMap.keySet());
-
+        // shows only the values
         System.out.println("values " + myMap.values());
-
         System.out.println(" print hashmap " + myMap);
-
+        // gets value at key entered
         System.out.println(myMap.get(11));
+        if(myMap.containsKey(11)){
+            myMap.remove(11);
+        }
+        System.out.println("Removed key 11");
+        System.out.println(myMap);
 
-
-
-
+        HashMap<String, Integer> myMap2 = new HashMap<>();
+        myMap2.put("mike", 99999);
+        System.out.println(myMap2);
+        System.out.println(myMap2.get("mike"));
     }
 }
