2feda49e03751de85efa6610c5492aba8f152577
G00407095@atu.ie <G00407095@atu.ie>
2024-10-16 14:53:48 +0100

    used loop to add first 10 values from 100 to 1000

--- src/main/java/ie/atu/practice/HashMapDemo.java
+++ src/main/java/ie/atu/practice/HashMapDemo.java
@@ -7,18 +7,13 @@ public class HashMapDemo {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Integer> myMap = new HashMap<>(10);
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        int baseValue = 100;
 
-        myMap.put(1, 100);
-        myMap.put(2, 200);
-        myMap.put(3, 300);
-        myMap.put(4, 400);
-        myMap.put(5, 500);
-        myMap.put(6, 600);
-        myMap.put(7, 700);
-        myMap.put(8, 800);
-        myMap.put(9, 900);
-        myMap.put(10, 1000);
+        for (int i = 1; i <= 10; i++){
+            myMap.put(i, baseValue);
+            baseValue += 100;
+        }
 
         System.out.println(" print hashmap " + myMap);
 
